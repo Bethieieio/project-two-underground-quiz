b6d83fd828b276463c64f52b8a25f27b798ddaee
Beth Allaway <beth_allaway@yahoo.com>
2022-08-13 18:34:12 +0100

    add radio buttons for answers. added start of submit event

---
+++ assets/js/script.js
@@ -0,0 +1,50 @@
+document.addEventListener("DOMContentLoaded", function() {
+    let form = document.getElementById('quiz')
+
+    form.addEventListener('submit', function(event) {
+        event.preventDefault()
+        
+    })
+})
+
+/**
+ * Sets game score to zero
+ * Sets game to question one
+ */
+function resetGame() {
+
+}
+
+/**
+ * Sends player to the next question
+ */
+function nextQuestion() {
+
+}
+/**
+ * Checks players answer
+ * marks answer
+ * shows correct answer if player's answer is incorrect
+ */
+function checkAnswer() {
+
+}
+
+/**
+ * Adds one point to players current score on DOM
+ */
+function incrementScore() {
+
+}
+
+/**
+ * ends the game
+ * congratulates player for doing well
+ * commiserates player if they did not do well
+ */
+function showResult() {
+
+}
+
+
+
