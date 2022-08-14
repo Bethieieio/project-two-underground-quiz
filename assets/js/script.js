b46d129c68600e547499694b1ce3b530690b7d29
Beth Allaway <beth_allaway@yahoo.com>
2022-08-14 20:43:24 +0100

    add form to get selected answser

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,50 +1,41 @@
-document.addEventListener("DOMContentLoaded", function() {
-    let form = document.getElementById('quiz')
+document.addEventListener("DOMContentLoaded", function () {
+  let form = document.getElementById("quiz");
 
-    form.addEventListener('submit', function(event) {
-        event.preventDefault()
-        
-    })
-})
+  // when answer is sent it runs this
+  form.addEventListener("submit", function (event) {
+    // prevents page from refreshing, when submit button is clicked.
+    event.preventDefault();
+    //selects answer player has selected
+    let formData = new FormData(event.target);
+    let selectedAnswer = formData.get("answer");
+  });
+});
 
 /**
  * Sets game score to zero
  * Sets game to question one
  */
-function resetGame() {
-
-}
+function resetGame() {}
 
 /**
  * Sends player to the next question
  */
-function nextQuestion() {
-
-}
+function nextQuestion() {}
 /**
  * Checks players answer
  * marks answer
  * shows correct answer if player's answer is incorrect
  */
-function checkAnswer() {
-
-}
+function checkAnswer() {}
 
 /**
  * Adds one point to players current score on DOM
  */
-function incrementScore() {
-
-}
+function incrementScore() {}
 
 /**
  * ends the game
  * congratulates player for doing well
  * commiserates player if they did not do well
  */
-function showResult() {
-
-}
-
-
-
+function showResult() {}
